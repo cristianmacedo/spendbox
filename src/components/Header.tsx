e21b1a96dc069030350bae9a3afae02d29afa143
Cristian Macedo <cristian.silvadk22@gmail.com>
2021-10-15 02:34:26 -0300

    refactor: header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,28 +3,27 @@ import React from 'react';
 import { FaGithub } from 'react-icons/fa';
 
 // eslint-disable-next-line @typescript-eslint/no-redeclare
-import { Box, Text, Button } from '@chakra-ui/react';
+import { Flex, Text, Button } from '@chakra-ui/react';
 
 import { APP_NAME, STYLE_SPACING } from 'config/constants';
 
 const Header = (): JSX.Element => (
-  <Box
+  <Flex
+    as="nav"
     w="100%"
     h="56px"
     bgColor="green.800"
-    display="flex"
-    alignItems="center"
-    justifyContent="space-between"
+    align="center"
+    justify="space-between"
     px={STYLE_SPACING}
   >
     <Text fontSize="lg" color="orange.300" fontWeight="bold">
       {APP_NAME}
     </Text>
-
     <Button rightIcon={<FaGithub />} color="orange.300" variant="link">
       Contribute
     </Button>
-  </Box>
+  </Flex>
 );
 
 export default Header;
