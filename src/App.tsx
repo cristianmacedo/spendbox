91ef19c682ba63de9890ea5ed2e6b0c7a01e6ebf
Cristian Macedo <cristian.silvadk22@gmail.com>
2021-10-20 01:59:17 -0300

    feat: character picker and balance indicator components integration

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,50 @@ import React from 'react';
 
 import { ChakraProvider } from '@chakra-ui/react';
 
-import Header from 'components/Header';
-
+import characters from 'config/mocks';
 import theme from 'config/theme';
 
-const App = (): JSX.Element => (
-  <ChakraProvider theme={theme}>
-    <Header />
-  </ChakraProvider>
-);
+import { CharacterValue } from 'domains/CharacterValue';
+
+import BalanceIndicator from 'components/BalanceIndicator';
+import CharacterPicker from 'components/CharacterPicker';
+import Header from 'components/Header';
+import Hero from 'components/Hero';
+
+const App = (): JSX.Element => {
+  const [selectedCharacter, setSelectedCharacter] =
+    React.useState<CharacterValue>();
+  const [startingBalance, setStartingBalance] = React.useState<number>(0);
+  const [currentBalance, setCurrentBalance] = React.useState<number>(0);
+
+  const handleCharacterChange = React.useCallback(
+    (character: CharacterValue) => {
+      setSelectedCharacter(character);
+      setCurrentBalance(character.netWorth - 1_000_000_000);
+      setStartingBalance(character.netWorth);
+    },
+    []
+  );
+
+  const subtitle = React.useMemo((): string => {
+    if (selectedCharacter) {
+      return `Spend ${selectedCharacter.name}'s fortune however you want on the available products below!`;
+    }
+    return 'Pick a billionaire and spend it’s fortune however you want on the available products below!';
+  }, [selectedCharacter]);
+
+  return (
+    <ChakraProvider theme={theme}>
+      <Header />
+      <Hero title="Spend billionaires money!" subtitle={subtitle}>
+        <CharacterPicker
+          characters={characters}
+          onCharacterChange={handleCharacterChange}
+        />
+      </Hero>
+      <BalanceIndicator start={startingBalance} end={currentBalance} />
+    </ChakraProvider>
+  );
+};
 
 export default App;
---
+++ src/domains/CharacterValue.ts
@@ -0,0 +1,7 @@
+export interface CharacterValue {
+  id: string;
+  avatar: string;
+  name: string;
+  source: string;
+  netWorth: number;
+}
