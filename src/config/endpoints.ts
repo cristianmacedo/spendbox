2b31f374764217e0a569b1c6a970ece87981a994
Cristian Macedo <cristian.silvadk22@gmail.com>
2023-07-19 13:34:16 -0300

    fix: forbes api endpoint

--- src/config/endpoints.ts
+++ src/config/endpoints.ts
@@ -1,2 +1,2 @@
-export const FORBES_URI = 'https://forbes400.herokuapp.com/api';
+export const FORBES_URI = "https://forbes400.onrender.com/api";
 export const FORBES_RTB_400 = `${FORBES_URI}/forbes400`;
