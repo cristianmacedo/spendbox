4b2db52c33c2f57d127745e46f84e7f7cdf13358
Cristian Macedo <cristian.silvadk22@gmail.com>
2026-01-02 01:54:08 -0300

    fix: output file tracing root path

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  outputFileTracingRoot: path.join(__dirname),
   images: {
     remotePatterns: [
       {
